48e060dec952cdc731a906f9d42f393e0d1dc3f0
frillas <roulleau.anthonin@orange.fr>
2025-01-19 12:54:33 +0100

    add printf && gnl

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aroullea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 09:01:38 by aroullea          #+#    #+#             */
-/*   Updated: 2024/10/19 15:10:10 by aroullea         ###   ########.fr       */
+/*   Updated: 2025/01/18 17:40:41 by aroullea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,7 +15,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*p;
 	size_t	n;
+	size_t	max_size_t;
 
+	max_size_t = (size_t)-1;
+	if (nmemb != 0 && size > max_size_t / nmemb)
+		return (NULL);
 	n = nmemb * size;
 	p = (void *) malloc(n);
 	if (p == NULL)
--- makefile
+++ makefile
@@ -15,7 +15,9 @@ ft_putendl_fd.c ft_putnbr_fd.c \
 
 BONUS = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c ft_lstlast_bonus.c \
 ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c ft_lstiter_bonus.c \
-ft_lstmap_bonus.c \
+ft_lstmap_bonus.c get_next_line/get_next_line.c get_next_line/get_next_line_utils.c \
+get_next_line/get_next_line_bonus.c get_next_line/get_next_line_utils_bonus.c \
+ft_printf/ft_list1.c ft_printf/ft_list2.c ft_printf/ft_printf.c \
 
 SRCS = $(SRC) $(BONUS)
 
